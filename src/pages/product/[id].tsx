e6559ffcbb64b7b0e3ce760a150989eedb48d388
Carlos Cassol <carloscassolfilho@gmail.com>
2023-02-23 20:53:39 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
 import { useRouter } from 'next/router';
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product';
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita saepe et eaque, ea qui adipisci placeat suscipit consequatur facere debitis dolorem! Adipisci voluptate assumenda provident ex recusandae! Dolores, ad ab.</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
